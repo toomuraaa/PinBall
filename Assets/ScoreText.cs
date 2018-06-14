1c2d21b43dad5804344bbfb687c1204eece401ef
toomuraaa <t.oomuraaa@gmail.com>
2018-06-14 15:40:35 +0900

    PinBallプロジェクト　初めてのコミット課題提出
    よろしくお願いします

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour {
+
+    public Text scoreText;
+    private int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string Tag = collision.gameObject.tag;
+
+        if(Tag == "LargeStarTag")
+        {
+            score += 10;
+        }else if(Tag == "SmallStarTag")
+        {
+            score += 5;
+        }else if(Tag == "LargeCloudTag")
+        {
+            score += 15;
+        }else if(Tag == "SmallCloudTag")
+        {
+            score += 10;
+        }
+        
+    }
+    // Update is called once per frame
+    void Update () {
+        this.scoreText.text = score.ToString();
+	}
+}
